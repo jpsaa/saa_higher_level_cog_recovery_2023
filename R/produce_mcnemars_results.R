7ff68341333f451a0b05d98258f6c0d0722572e3
Saras Windecker <sm.windecker@unimelb.edu.au>
2020-07-26 21:58:41 +1000

    change argument to abstract

--- R/produce_mcnemars_results.R
+++ R/produce_mcnemars_results.R
@@ -1,12 +1,12 @@
 #### McNemars tests
-produce_mcnemars_results <- function (moca_with_impaired) {
+produce_mcnemars_results <- function (data) {
   
   change1 <- matrix(
     c(
-      all.moca %>% filter(imp.base == "yes", imp.mo3 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.base == "no", imp.mo3 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.base == "yes", imp.mo3 == "no") %>% nrow(),
-      all.moca %>% filter(imp.base == "no", imp.mo3 == "no") %>% nrow()),
+      data %>% filter(imp.base == "yes", imp.mo3 == "yes") %>% nrow(),
+      data %>% filter(imp.base == "no", imp.mo3 == "yes") %>% nrow(),
+      data %>% filter(imp.base == "yes", imp.mo3 == "no") %>% nrow(),
+      data %>% filter(imp.base == "no", imp.mo3 == "no") %>% nrow()),
     dimnames = list (`week 1` = c("Cognitive impairment", "No cognitive impairment"),
                      `month 3` = c("Cognitive impairment", "No cognitive impairment")),
     nrow = 2) %>% 
@@ -14,10 +14,10 @@ produce_mcnemars_results <- function (moca_with_impaired) {
   
   change2 <- matrix(
     c(
-      all.moca %>% filter(imp.mo3 == "yes", imp.mo12 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.mo3 == "no", imp.mo12 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.mo3 == "yes", imp.mo12 == "no") %>% nrow(),
-      all.moca %>% filter(imp.mo3 == "no", imp.mo12 == "no") %>% nrow()),
+      data %>% filter(imp.mo3 == "yes", imp.mo12 == "yes") %>% nrow(),
+      data %>% filter(imp.mo3 == "no", imp.mo12 == "yes") %>% nrow(),
+      data %>% filter(imp.mo3 == "yes", imp.mo12 == "no") %>% nrow(),
+      data %>% filter(imp.mo3 == "no", imp.mo12 == "no") %>% nrow()),
     dimnames = list (`month 3` = c("Cognitive impairment", "No cognitive impairment"),
                      `month 12` = c("Cognitive impairment", "No cognitive impairment")),
     nrow = 2) %>% 
@@ -25,10 +25,10 @@ produce_mcnemars_results <- function (moca_with_impaired) {
   
   overall_change <- matrix(
     c(
-      all.moca %>% filter(imp.base == "yes", imp.mo12 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.base == "no", imp.mo12 == "yes") %>% nrow(),
-      all.moca %>% filter(imp.base == "yes", imp.mo12 == "no") %>% nrow(),
-      all.moca %>% filter(imp.base == "no", imp.mo12 == "no") %>% nrow()),
+      data %>% filter(imp.base == "yes", imp.mo12 == "yes") %>% nrow(),
+      data %>% filter(imp.base == "no", imp.mo12 == "yes") %>% nrow(),
+      data %>% filter(imp.base == "yes", imp.mo12 == "no") %>% nrow(),
+      data %>% filter(imp.base == "no", imp.mo12 == "no") %>% nrow()),
     dimnames = list (`week 1` = c("Cognitive impairment", "No cognitive impairment"),
                      `month 12` = c("Cognitive impairment", "No cognitive impairment")),
     nrow = 2) %>% 
