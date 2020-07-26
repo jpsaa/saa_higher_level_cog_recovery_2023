f60ea299804eda03019d7c81d069d1aea9786020
Saras Windecker <sm.windecker@unimelb.edu.au>
2020-07-26 21:57:24 +1000

    simplify code and change argument to abstract object

--- R/add_impaired.R
+++ R/add_impaired.R
@@ -1,9 +1,15 @@
-add_impaired <- function (all.moca) {
+add_impaired <- function (data) {
   
   ##### Mcnemars test for count data ####
-  all.moca$imp.base <- all.moca %>% mutate(imp.base=ifelse(moca_score_w1 < 24, "yes","no")) %>% select(imp.base)
-  all.moca$imp.mo3 <- all.moca %>% mutate(imp.mo3=ifelse(moca_score_mo3 < 24, "yes","no")) %>% select(imp.mo3)
-  all.moca$imp.mo12 <- all.moca %>% mutate(imp.mo12=ifelse(moca_score_mo12 < 24, "yes","no")) %>% select(imp.mo12)
+  data$imp.base <- data %>% 
+    mutate(imp.base = ifelse(moca_score_w1 < 24, "yes", "no")) %>% 
+    select(imp.base)
+  data$imp.mo3 <- data %>% 
+    mutate(imp.mo3 = ifelse(moca_score_mo3 < 24, "yes", "no")) %>% 
+    select(imp.mo3)
+  data$imp.mo12 <- data %>% 
+    mutate(imp.mo12 = ifelse(moca_score_mo12 < 24, "yes", "no")) %>% 
+    select(imp.mo12)
 
-  all.moca
+  data
 }
