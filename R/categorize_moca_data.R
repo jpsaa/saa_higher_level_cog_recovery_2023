1996dd25b033eab14b2ff4c74e96a9b6fd3ead55
jpsaa <saajp@outlook.com>
2020-11-20 18:32:09 +1100

    All code (not simplified)

--- R/categorize_moca_data.R
+++
@@ -1,8 +0,0 @@
-categorize_moca_data <- function (data) {
-  
-  data.impaired <- data %>% 
-    mutate(impaired_w1 = ifelse(moca_score_w1 < 24, 1, 0)) %>% 
-    mutate(impaired_mo3 = ifelse(moca_score_mo3 < 24, 1, 0)) %>% 
-    mutate(impaired_mo12 = ifelse(moca_score_mo12 < 24, 1, 0))
-   
-}
--- R/cross_validate.R
+++
@@ -1,7 +0,0 @@
-### cross validation function
-cross_validate <- function (combination, data) {
-  
-  all_ids <- unique(data$id)
-  lapply(all_ids, fit_model, combination, data)
-  
-}
